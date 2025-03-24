59767cca14b1aaa129feb0c61314fcd90d26abb9
Philip Modayil <philipv.modayil@gmail.com>
2025-03-24 12:15:27 +0100

    Created GA class and constructor

--- genetic_algorithm.cpp
+++ genetic_algorithm.cpp
@@ -3,4 +3,12 @@
 
 namespace GA{
 
+    // Constructor
+    GeneticAlgorithm::GeneticAlgorithm(std::vector<double> starting_curve, int population_size, int num_generations, double mutation_rate)
+    : starting_curve(starting_curve), population_size(population_size), num_generations(num_generations), mutation_rate(mutation_rate) {}
+
+
+    
+    
+
 } // end of namespace
--- genetic_algorithm.h
+++ genetic_algorithm.h
@@ -4,5 +4,17 @@
 #include <vector>
 namespace GA{
 
+    class GeneticAlgorithm {
+        private:
+            std::vector<double> starting_curve;
+            const int population_size;
+            const int num_generations;
+            const double mutation_rate;
+
+        public:
+            // Constructor
+            GeneticAlgorithm(std::vector<double> starting_curve, int population_size, int num_generations, double mutation_rate);
+    };
+
 } // end of namespace
 #endif
